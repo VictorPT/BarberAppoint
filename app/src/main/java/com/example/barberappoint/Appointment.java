3e806781bc6b8cf0814f4637ce1de99c0c6c72c7
Victor Pena <victor.pentaveras@verizon.com>
2019-10-24 16:57:48 -0400

    Commit inicial de proyecto con la parte de escritura a la base de datos de Firebase

---
+++ app/src/main/java/com/example/barberappoint/Appointment.java
@@ -0,0 +1,47 @@
+package com.example.barberappoint;
+
+public class Appointment {
+    private String name;
+    private String service;
+    private String date;
+    private String hour;
+
+    public Appointment(String name, String service, String date, String hour) {
+        this.setName(name);
+        this.setService(service);
+        this.setDate(date);
+        this.setHour(hour);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+}
---
+++ app/src/main/java/com/example/barberappoint/InsertActivity.java
@@ -0,0 +1,65 @@
+package com.example.barberappoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class InsertActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    EditText txtName;
+    EditText txtService;
+    EditText txtDate;
+    EditText txtHour;
+    Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("appointment");
+
+        txtName = findViewById(R.id.txtName);
+        txtService = findViewById(R.id.txtService);
+        txtDate = findViewById(R.id.txtDate);
+        txtHour = findViewById(R.id.txtHour);
+        btnSave = findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSave:
+                saveAppointment();
+                Toast.makeText(this, "Cita guardada", Toast.LENGTH_LONG).show();
+                clean();
+                break;
+        }
+    }
+
+    private void saveAppointment(){
+        String name = txtName.getText().toString();
+        String service = txtService.getText().toString();
+        String date = txtDate.getText().toString();
+        String hour = txtHour.getText().toString();
+        Appointment appoint = new Appointment(name, service, date, hour);
+        mDatabaseReference.push().setValue(appoint);
+    }
+    private void clean() {
+        txtName.setText("");
+        txtService.setText("");
+        txtDate.setText("");
+        txtHour.setText("");
+    }
+}
